85b550066bdf5fc8985f67772fbe35767b8d06e9
vitalyionov <vitalyionov@gmail.com>
2021-01-24 18:03:41 +0300

    module2-task3

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const server = require(`./server`);
 const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
 
 const Cli = {
+  [server.name]: server,
   [generate.name]: generate,
   [help.name]: help,
-  [version.name]: version,
+  [version.name]: version
 };
 
 module.exports = {
