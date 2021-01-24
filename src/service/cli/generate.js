e65a5994b90bfbd96aa604e33908a43f59bda8e7
vitalyionov <vitalyionov@gmail.com>
2021-01-24 17:26:31 +0300

    module2-task2

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -10,36 +10,39 @@ const {
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
+const FILE_SENTENCES_PATH = `src/data/sentences.txt`;
+const FILE_TITLES_PATH = `src/data/titles.txt`;
+const FILE_CATEGORIES_PATH = `src/data/categories.txt`;
 
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
+// const TITLES = [
+//   `Продам книги Стивена Кинга`,
+//   `Продам новую приставку Sony Playstation 5`,
+//   `Продам отличную подборку фильмов на VHS`,
+//   `Куплю антиквариат`,
+//   `Куплю породистого кота`,
+// ];
 
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
+// const SENTENCES = [
+//   `Товар в отличном состоянии.`,
+//   `Пользовались бережно и только по большим праздникам.`,
+//   `Продаю с болью в сердце...`,
+//   `Бонусом отдам все аксессуары.`,
+//   `Даю недельную гарантию.`,
+//   `Если товар не понравится — верну всё до последней копейки.`,
+//   `Это настоящая находка для коллекционера!`,
+//   `Если найдёте дешевле — сброшу цену.`,
+//   `Таких предложений больше нет!`,
+//   `При покупке с меня бесплатная доставка в черте города.`,
+// ];
 
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
+// const CATEGORIES = [
+//   `Книги`,
+//   `Разное`,
+//   `Посуда`,
+//   `Игры`,
+//   `Животные`,
+//   `Журналы`,
+// ];
 
 const OfferType = {
   OFFER: `offer`,
@@ -70,18 +73,32 @@ const getPictureFileName = (number) => {
   return `item${getPictureItemNumber(number)}.jpg`;
 };
 
-const generateOffers = (count) => {
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generateOffers = (count, titles, categories, sentences) => {
   return Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+    description: shuffle(sentences).slice(1, 5).join(` `),
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+    category: [categories[getRandomInt(0, categories.length - 1)]],
   }));
 };
 
 const runGenerateData = async (args) => {
+  const sentences = await readContent(FILE_SENTENCES_PATH);
+  const titles = await readContent(FILE_TITLES_PATH);
+  const categories = await readContent(FILE_CATEGORIES_PATH);
+
   const [count] = args;
   const countData = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
@@ -89,13 +106,13 @@ const runGenerateData = async (args) => {
     return console.error(chalk.red(`Не больше 1000 объявлений`));
   }
 
-  const content = JSON.stringify(generateOffers(countData));
+  const content = JSON.stringify(generateOffers(countData, titles, categories, sentences));
 
   try {
     await fs.writeFile(FILE_NAME, content);
-    console.error(chalk.red(`Can't write data to file...`));
-  } catch (err) {
     console.info(chalk.green(`Operation success. File created.`));
+  } catch (err) {
+    console.error(chalk.red(`Can't write data to file...`));
   }
 
   return null;
