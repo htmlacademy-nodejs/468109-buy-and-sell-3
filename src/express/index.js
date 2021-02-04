059f34b343a24f60d4b7abafa1a4bc8b750dbb4a
vitalyionov <vitalyionov@gmail.com>
2021-02-04 21:28:20 +0300

    module3-task1

---
+++ src/express/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require(`express`);
+
+const mainRoutes = require(`./routes/main-routes`);
+const registerRoutes = require(`./routes/register-routes`);
+const loginRoutes = require(`./routes/login-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const offersRoutes = require(`./routes/offers-routes`);
+const searchRoutes = require(`./routes/search-routes`);
+
+const DEFAULT_PORT = 8080;
+
+const app = express();
+
+app.use(`/`, mainRoutes);
+app.use(`/register`, registerRoutes);
+app.use(`/login`, loginRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/offers`, offersRoutes);
+app.use(`/search`, searchRoutes);
+
+app.listen(DEFAULT_PORT);
