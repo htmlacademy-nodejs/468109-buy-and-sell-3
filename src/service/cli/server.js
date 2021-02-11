43d3167fe6f56fd31ddcf28133c0053dc7e1e253
vitalyionov <vitalyionov@gmail.com>
2021-02-11 19:15:36 +0300

    module3-task3

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const chalk = require(`chalk`);
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
 
 const {
@@ -11,58 +10,28 @@ const {
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILENAME);
-        const mocks = JSON.parse(fileContent);
-        const messages = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${messages}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-  }
-};
-
 const runServer = (args) => {
   const [customPort] = args;
   const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+  const app = express();
 
-  http.createServer(onClientConnect)
-    .listen(port)
-    .on(`listening`, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Ошибка при создании сервера`, err));
-      }
+  app.use(express.json());
 
-      return console.info(chalk.green(`Ожидаю соединение на ${port}`));
-    });
+  app.get(`/offers`, async (req, res) => {
+    try {
+      const fileContent = await fs.readFile(FILENAME);
+      const mocks = JSON.parse(fileContent);
+      res.json(mocks);
+    } catch (err) {
+      res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+    }
+  });
+
+  app.use((req, res) => res
+    .status(HttpCode.NOT_FOUND)
+    .send(`Not found`));
+
+  app.listen(port);
 };
 
 module.exports = {
