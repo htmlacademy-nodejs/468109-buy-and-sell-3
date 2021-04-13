8bad1177e7ac1c574aa6bdbfbcb6082706195965
vitalyionov <vitalyionov@gmail.com>
2021-04-13 16:10:38 +0300

    Вносит правки в соответствии с замечаниями

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,8 @@
 module.exports.DEFAULT_COMMAND = `--help`;
 module.exports.USER_ARGV_INDEX = 2;
 module.exports.ExitCode = {
-  success: 0,
-  error: 1
+  SUCCESS: 0,
+  ERROR: 1
 };
 module.exports.MAX_ID_LENGTH = 6;
 module.exports.Env = {
--- src/service/api/offer.e2e.test.js
+++ src/service/api/offer.e2e.test.js
@@ -6,127 +6,12 @@ const {StatusCodes} = require(`http-status-codes`);
 
 const offer = require(`./offer`);
 const DataService = require(`../data-service/offer`);
-
-const mockData = [
-  {
-    id: `UQtEAB`,
-    title: `Куплю торт`,
-    picture: `item13.jpg`,
-    description: `Даю недельную гарантию. Пользовались бережно и только по большим праздникам. Таких предложений больше нет! Бонусом отдам все аксессуары.`,
-    type: `OFFER`,
-    sum: 2619,
-    category: [
-      `Словоблудие`
-    ],
-    comments: [
-      {
-        id: `Usvy8J`,
-        text: `С чем связана продажа? Почему так дешёво?`
-      },
-      {
-        id: `vzO9gL`,
-        text: `А сколько игр в комплекте? Почему в таком ужасном состоянии? Продаю в связи с переездом. Отрываю от сердца.`
-      }
-    ]
-  },
-  {
-    id: `ZZCcei`,
-    title: `Куплю антиквариат`,
-    picture: `item07.jpg`,
-    description: `Бонусом отдам все аксессуары. Если найдёте дешевле — сброшу цену. Пикачу способен высвобождать электрическую энергию Это настоящая находка для коллекционера!`,
-    type: `OFFER`,
-    sum: 52326,
-    category: [
-      `Игры`
-    ],
-    comments: [
-      {
-        id: `ZxU62o`,
-        text: `Продаю в связи с переездом. Отрываю от сердца. А где блок питания? Оплата наличными или перевод на карту?`
-      },
-      {
-        id: `rw6p8C`,
-        text: `С чем связана продажа? Почему так дешёво? А сколько игр в комплекте?`
-      }
-    ]
-  },
-  {
-    id: `Pdeh--`,
-    title: `Продам новую приставку Sony Playstation 5`,
-    picture: `item07.jpg`,
-    description: `Этот покемон достаточно своевольный Товар в отличном состоянии. Если использовать на Пикачу Камень Молнии, то он превратится в Райчу Пользовались бережно и только по большим праздникам.`,
-    type: `SALE`,
-    sum: 14733,
-    category: [
-      `Наркотики`
-    ],
-    comments: [
-      {
-        id: `sz1GGb`,
-        text: `Продаю в связи с переездом. Отрываю от сердца. Оплата наличными или перевод на карту? С чем связана продажа? Почему так дешёво?`
-      },
-      {
-        id: `z4p879`,
-        text: `Неплохо, но дорого. Совсем немного...`
-      }
-    ]
-  },
-  {
-    id: `Q0PlwA`,
-    title: `Куплю торт`,
-    picture: `item11.jpg`,
-    description: `Таких предложений больше нет! Если товар не понравится — верну всё до последней копейки. Бонусом отдам все аксессуары. При покупке с меня бесплатная доставка в черте города.`,
-    type: `SALE`,
-    sum: 4978,
-    category: [
-      `Словоблудие`
-    ],
-    comments: [
-      {
-        id: `7QzIJ0`,
-        text: `А сколько игр в комплекте?`
-      },
-      {
-        id: `Sc2ace`,
-        text: `Вы что?! В магазине дешевле. `
-      },
-      {
-        id: `hQtksw`,
-        text: `Совсем немного...`
-      },
-      {
-        id: `t2wV7y`,
-        text: `Вы что?! В магазине дешевле.`
-      }
-    ]
-  },
-  {
-    id: `pG37PI`,
-    title: `Продам отличную подборку фильмов на VHS`,
-    picture: `item03.jpg`,
-    description: `Если товар не понравится — верну всё до последней копейки. Продаю с болью в сердце... Товар в отличном состоянии. Если найдёте дешевле — сброшу цену.`,
-    type: `SALE`,
-    sum: 99908,
-    category: [
-      `Игры`
-    ],
-    comments: [
-      {
-        id: `_BThzF`,
-        text: `Совсем немного...`
-      },
-      {
-        id: `wRTM5L`,
-        text: `А сколько игр в комплекте?`
-      }
-    ]
-  }
-];
+const {mockOffers} = require(`../constants/mocksData`);
 
 const createAPI = () => {
   const app = express();
 
-  const cloneData = JSON.parse(JSON.stringify(mockData));
+  const cloneData = JSON.parse(JSON.stringify(mockOffers));
 
   app.use(express.json());
   offer(app, new DataService(cloneData));
@@ -360,7 +245,11 @@ describe(`API correctly deletes a comment`, () => {
       .delete(`/offers/UQtEAB/comments/Usvy8J`);
   });
 
-  test(`Status code 200`, () => expect(response.statusCode).toBe(StatusCodes.OK));
+  test(`Status code 200`, () => {
+    // console.log('response', response);
+
+    return expect(response.statusCode).toBe(StatusCodes.OK);
+  });
 
   test(`Comments count is 1 now`, () => request(app)
     .get(`/offers/UQtEAB/comments`)
