6b33ccdae6086eabd867fcf16c0f2726f5f6bf7c
vitalyionov <ionovvi@mos.ru>
2021-06-19 10:27:16 +0300

    Исправляет замечания наставника

--- src/express/middlewares/upload.js
+++ src/express/middlewares/upload.js
@@ -7,6 +7,8 @@ const {nanoid} = require(`nanoid`);
 const UPLOAD_DIR = `../upload/img/`;
 
 const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
+const maxSize = 1000 * 1000 * 32;
+const allowedMimeTypes = new Set([`image/png`, `image/jpg`, `image/jpeg`]);
 
 const storage = multer.diskStorage({
   destination: uploadDirAbsolute,
@@ -17,6 +19,18 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({storage});
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: maxSize
+  },
+  fileFilter: (req, file, callback) => {
+    if (allowedMimeTypes.has(file.mimetype)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error(`Allowed mimetypes is only "${[...allowedMimeTypes].toString()}"`));
+  },
+});
 
 module.exports = upload;
